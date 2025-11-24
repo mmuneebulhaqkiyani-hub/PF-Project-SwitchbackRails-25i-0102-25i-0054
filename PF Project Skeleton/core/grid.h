20ca6aa1f3bc32cedcfb019dfdf031dd4cd9bc43
saadinam-lab <saadinamphone123@gmail.com>
2025-11-24 10:05:09 +0000

    dk

--- PF Project Skeleton/core/grid.h
+++ PF Project Skeleton/core/grid.h
@@ -17,7 +17,7 @@ bool isTrackTile(int x, int y);
 bool isSwitchTile(char m);
 
 // Get the switch index (0-25) from a switch character (A-Z)
-int getSwitchIndex(char y);
+int getSwitchIndex(char x);
 
 // Check if a position is a spawn point
 bool isSpawnPoint();
