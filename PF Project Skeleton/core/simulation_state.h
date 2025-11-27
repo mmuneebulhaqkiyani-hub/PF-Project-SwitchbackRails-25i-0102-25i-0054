b84bd859baf511be6d5bb93b25423532b37bb2bd
saadinam-lab <saadinamphone123@gmail.com>
2025-11-27 20:19:05 +0000

    done the basic variable declaration on simstate.h thing there may be some undeclared stuffcuz me brain is barely working will revise it tomorrow

--- PF Project Skeleton/core/simulation_state.h
+++ PF Project Skeleton/core/simulation_state.h
@@ -45,22 +45,37 @@
 // ----------------------------------------------------------------------------
 //now we should have constants for the switches too
 //but S and D are not switches so we have to exclude them but confused on this rn if we should keep it 26 or nah lets just do 26 we will change this if necessary
- const int Maxswitches=26; // A-Z means 26 switches max
+ const int Maxswitches=26;// A-Z means 26 switches max
 //each level will have specific number of switches so we need a variable for that too
  extern int g_Switchcurrent; // current number of switches in the level
  //we need x and y position arrays for the switches too
  extern int g_switchX[Maxswitches]; // x position for each switch
 extern int g_switchY[Maxswitches]; // y position for each switch
 
+extern int  g_switchState[Maxswitches];// current state (0 = straight, 1 = turn)
+
+extern int  g_switchInitState[Maxswitches]; // initial state from level file
+extern int  g_switchMode[Maxswitches];// 0 = PER_DIR, 1 = GLOBAL
+extern int  g_switchCounterUp[Maxswitches];
+extern int  g_switchCounterRight[Maxswitches];
+extern int  g_switchCounterDown[Maxswitches];
+extern int  g_switchCounterLeft[Maxswitches];
+extern bool g_switchQueueFlip[Maxswitches];// ts wold be true if switch should flip at end of tick so like if tick is 5 and we queue flip it then at the end of tick 5 it will flip and be effective for tick 6 (hopefully)
+
+
 // ----------------------------------------------------------------------------
 // WEATHER CONSTANTS
 // ----------------------------------------------------------------------------
 
-
+const int WeatherNormal = 0;
+const int WeatherRain   = 1;
+const int WeatherFog    = 2;
 // ----------------------------------------------------------------------------
 // SIGNAL CONSTANTS
 // ----------------------------------------------------------------------------
-
+const int SignalRed    = 0;
+const int SignalYellow = 1;
+const int SignalGreen  = 2;
 
 
 
@@ -68,10 +83,13 @@ extern int g_switchY[Maxswitches]; // y position for each switch
 // GLOBAL STATE: GRID
 // ----------------------------------------------------------------------------
 
+//no need to do nothing fr cuz we already declared the g_variables above
+
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: TRAINS
 // ----------------------------------------------------------------------------
-extern int g_tickNumber; // this is the current simulation tick 
+extern int g_tickNumber; // this will be  the current simulation tick 
+
 
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: SWITCHES (A-Z mapped to 0-25)
@@ -87,21 +105,35 @@ extern int  g_trainSpawnTick[MaxTrains];
 // GLOBAL STATE: DESTINATION POINTS
 // ----------------------------------------------------------------------------
 
+//ok so we have to set like the max number of D (destination) points too
+const int MaxDestinations = 50;  
+extern int g_numdestinations;//ts wil tell us how many destination tiles in the map
+extern int g_destinationX[MaxDestinations];
+extern int g_destinationY[MaxDestinations];
 
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: SIMULATION PARAMETERS
 // ----------------------------------------------------------------------------
-
+extern int g_weatherMode;// here its gonna be for like various modes like weather normal / weather rain / weather fog
+extern int g_randomSeed;// this is the seed for therandom behaviour thing
 
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: METRICS
 // ----------------------------------------------------------------------------
+extern int g_totalTicksRun;//ok so this will tell us how many ticks are done in the simulation thing
 
+
+extern int g_trainsArrived;//ts tells how many trains that reached destination
+extern int g_trainsCrashed;// ts the trains that crashed thing
 
 // ----------------------------------------------------------------------------
 // GLOBAL STATE: EMERGENCY HALT
 // ----------------------------------------------------------------------------
-
+//so we gonna need bool to see if the emergency halt zone is active or no it will be useful when we implement the emergency halt feature and extern is becz it will be in the .cpp file
+extern bool g_emergencyActive;
+extern int  g_emergencyCenterX;// center x of 3x3 emergency zone
+extern int  g_emergencyCenterY;// center y of 3x3 emergency zone
+extern int  g_emergencyTicksRemaining;// ticks left for halt
 
 // ----------------------------------------------------------------------------
 // INITIALIZATION FUNCTION
